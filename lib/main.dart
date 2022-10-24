37ee9df2ca77d06e0596bbc1edd826a407aea1ee
dme-environment <yang-tianshu@mki.co.jp>
2022-10-24 16:21:51 +0900

    tag version 1.0

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ void main() async {
 
   // Get any initial links
   final PendingDynamicLinkData? initialLink = await FirebaseDynamicLinks.instance.getInitialLink();
-// 12345
+// 123456
   runApp(const MyApp());
 }
 
